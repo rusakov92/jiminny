b68311848309993de5e6b56515aad374428846bc
Aleksandar Rusakov <aleksandar.rusakov@rewardgateway.com>
2021-04-24 23:46:57 +0300

    Adding missing php dependencies

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,11 @@ ENV PATH="${PATH}:/root/.composer/vendor/bin"
 
 RUN apk --no-cache add php8 php8-fpm php8-json php8-curl \
     php8-zlib php8-xml php8-phar php8-intl php8-dom php8-xmlreader php8-ctype php8-session \
-    php8-mbstring php8-gd php8-pecl-xdebug php8-iconv \
-    php8-simplexml php8-tokenizer php8-pecl-apcu \
-    py3-setuptools nginx supervisor curl git bash vim composer
+    php8-mbstring php8-gd php8-pecl-xdebug php8-iconv php8-xmlwriter php8-fileinfo \
+    php8-simplexml php8-tokenizer php8-pecl-apcu php8-openssl \
+    py3-setuptools nginx supervisor curl git bash vim
 
+RUN ln -s /usr/bin/php8 /usr/bin/php
 RUN rm /etc/nginx/conf.d/default.conf
 
 COPY docker/nginx/nginx.conf /etc/nginx/nginx.conf
--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-#/usr/bin/composer install --no-interaction --no-scripts --working-dir /jiminny --optimize-autoloader --apcu-autoloader
+EXPECTED_CHECKSUM="$(/usr/bin/php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+/usr/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(/usr/bin/php -r "echo hash_file('sha384', 'composer-setup.php');")"
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+/usr/bin/php composer-setup.php --install-dir="/usr/bin" --filename="composer"
+rm composer-setup.php
+
+/usr/bin/composer install --no-interaction --working-dir /jiminny --optimize-autoloader --apcu-autoloader
 
 /usr/bin/supervisord -c /etc/supervisord.conf -n
